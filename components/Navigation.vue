c154305538db0c4bdcf8999504d4ead88f192035
Chris Calo <chris.calo@gmail.com>
2024-03-15 01:12:07 -0400

    Add `<Navigation/>` component which doesn't use client-side routing
    Fixes #4

---
+++ components/Navigation.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <nav>
+    <a href="/">Home</a>
+    <a href="/foo">Foo</a>
+  </nav>
+</template>
+
+<style scoped></style>
---
+++ pages/foo/index.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <Navigation/>
+  <div>
+    Page: foo/index
+  </div>
+</template>
+
+<style scoped></style>
